9484058b15043150965977af563026d5c5f7fbc5
cr4shed <63892494+cr4shed@users.noreply.github.com>
2023-03-08 01:18:39 -0500

    Added DbContextFactory and skeleton services

---
+++ BadgeTracker/Data/Database/DBContexts/DbContextFactory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BadgeTracker.Data
+{
+    public class DbContextFactory
+    {
+        private static DbContextOptions dbContextOptions = null;
+
+        /// <summary>
+        /// Set the connection string for the DB context factory. 
+        /// Created contexts will use this connection string. 
+        /// </summary>
+        /// <param name="connectionStringID">The connection string ID in app settings</param>
+        public static void SetConnectionString(string connectionStringID)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+
+            dbContextOptions = new DbContextOptionsBuilder<TrackerDbContext>()
+                .UseSqlite(configuration.GetConnectionString(connectionStringID))
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+                .Options;
+        }
+
+        /// <summary>
+        /// Create a new instance of  TrackerDbContext.
+        /// Returns null if no connection string has been set.
+        /// </summary>
+        /// <returns>New instance of TrackerDbContext.</returns>
+        public static TrackerDbContext CreateInstance()
+        {
+            if (dbContextOptions == null) return null;
+
+            return new TrackerDbContext(dbContextOptions);
+        }
+    }
+}
---
+++ BadgeTracker/Data/Database/Services/Interfaces/IEarnablesService.cs
@@ -0,0 +1,11 @@
+﻿namespace BadgeTracker.Data
+{
+    public interface IEarnablesService
+    {
+        Task<Activity> GetAllActivities();
+        Task<Badge> GetAllBadges();
+        Task<EarnedEarnable> GetCompletedEarnablesByUserId(int userId);
+        Task<EarnedBadge> GetCompletedBadgesByBadgeId(int badgeId);
+        Task<CompletedActivity> GetCompletedActivitiesByActivityId(int activityId);
+    }
+}
